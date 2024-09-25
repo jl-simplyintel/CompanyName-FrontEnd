16e21636555e2627b0189b33caa279f7deda8b26
jl-simplyintel <josiah@simplyintel.com>
2024-09-26 04:54:26 +0800

    Added Breadcrumbs to Quote Page

--- components/Breadcrumbs_Quote.js
+++ components/Breadcrumbs_Quote.js
@@ -2,46 +2,46 @@ import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 
 const Breadcrumbs_Quote = ({ businessName }) => {
-    const router = useRouter();  // Ensure router is properly initialized here
-    const [business, setBusiness] = useState('');
+  const router = useRouter();  // Ensure router is properly initialized here
+  const [business, setBusiness] = useState('');
 
-    useEffect(() => {
-        if (businessName) {
-            setBusiness(businessName);
-        }
-    }, [businessName]);
+  useEffect(() => {
+    if (businessName) {
+      setBusiness(businessName);
+    }
+  }, [businessName]);
 
-    return (
-        <nav className="breadcrumb">
-            <ul className="flex space-x-2">
-                <li>
-                    <Link href="/">
-                        <span className="text-blue-500">Home</span>
-                    </Link>
-                </li>
-                <li>
-                    <span className="mx-2">/</span>
-                    <Link href="/businesses">
-                        <span className="text-blue-500">Businesses</span>
-                    </Link>
-                </li>
-                {business && (
-                    <>
-                        <li>
-                            <span className="mx-2">/</span>
-                            <Link href={`/business/${router.query.id}`}> {/* Using router.query.id */}
-                                <span className="text-blue-500">{business}</span>
-                            </Link>
-                        </li>
-                        <li>
-                            <span className="mx-2">/</span>
-                            <span>Get a Quote</span>
-                        </li>
-                    </>
-                )}
-            </ul>
-        </nav>
-    );
+  return (
+    <nav className="breadcrumb">
+      <ul className="flex space-x-2">
+        <li>
+          <Link href="/">
+            <span className="text-blue-500">Home</span>
+          </Link>
+        </li>
+        <li>
+          <span className="mx-2">/</span>
+          <Link href="/businesses">
+            <span className="text-blue-500">Businesses</span>
+          </Link>
+        </li>
+        {business && (
+          <>
+            <li>
+              <span className="mx-2">/</span>
+              <Link href={`/business/${router.query.id}`}> {/* Using router.query.id */}
+                <span className="text-blue-500">{business}</span>
+              </Link>
+            </li>
+            <li>
+              <span className="mx-2">/</span>
+              <span>Get a Quote</span>
+            </li>
+          </>
+        )}
+      </ul>
+    </nav>
+  );
 };
 
 export default Breadcrumbs_Quote;
