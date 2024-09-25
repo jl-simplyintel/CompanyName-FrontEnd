b7ce3245164cfd039958ce2cfe3d4a1aaf963b02
jl-simplyintel <josiah@simplyintel.com>
2024-09-25 12:22:24 +0800

    Converted next.config.js tonext.config.mjs - Ran new build

---
+++ next.config.mjs
@@ -0,0 +1,22 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/home',
+        permanent: true,
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/sitemap.xml',
+        destination: '/sitemap',
+      },
+    ];
+  },
+};
+
+export default nextConfig;
