605694854f81e5ca538bc0648c6ec40b22c01079
jl-simplyintel <josiah@simplyintel.com>
2024-09-26 04:36:04 +0800

    Added Breadcrumbs to Quote Page

--- components/Breadcrumbs_Quote.js
+++ components/Breadcrumbs_Quote.js
@@ -1,21 +1,6 @@
-// components\Breadcrumbs_Quote.js
 import Link from 'next/link';
-import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
 
 const Breadcrumbs_Quote = ({ businessName }) => {
-    const router = useRouter();
-    const pathArray = router.pathname.split('/').filter((path) => path);
-
-    // Set the breadcrumb label for business page
-    const [business, setBusiness] = useState('');
-
-    useEffect(() => {
-        if (businessName) {
-            setBusiness(businessName);
-        }
-    }, [businessName]);
-
     return (
         <nav className="breadcrumb">
             <ul className="flex space-x-2">
@@ -24,28 +9,24 @@ const Breadcrumbs_Quote = ({ businessName }) => {
                         <span className="text-blue-500">Home</span>
                     </Link>
                 </li>
-                {pathArray.includes('business') && (
+                <li>
+                    <span className="mx-2">/</span>
+                    <Link href="/businesses">
+                        <span className="text-blue-500">Businesses</span>
+                    </Link>
+                </li>
+                {businessName && (
                     <>
                         <li>
                             <span className="mx-2">/</span>
-                            <Link href="/businesses">
-                                <span className="text-blue-500">Businesses</span>
+                            <Link href={`/business/${router.query.id}`}>
+                                <span className="text-blue-500">{business}</span>
                             </Link>
                         </li>
-                        {business && (
-                            <>
-                                <li>
-                                    <span className="mx-2">/</span>
-                                    <Link href={`/business/${router.query.id}`}>
-                                        <span className="text-blue-500">{business}</span>
-                                    </Link>
-                                </li>
-                                <li>
-                                    <span className="mx-2">/</span>
-                                    <span>Get a Quote</span>
-                                </li>
-                            </>
-                        )}
+                        <li>
+                            <span className="mx-2">/</span>
+                            <span>Get a Quote</span>
+                        </li>
                     </>
                 )}
             </ul>
