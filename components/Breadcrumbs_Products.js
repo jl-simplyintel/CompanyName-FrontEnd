1c94021c7a3f2feee9b4e431436058e8b2e4f1a0
jl-simplyintel <josiah@simplyintel.com>
2024-09-27 00:14:35 +0800

    Products Page Added

--- components/Breadcrumbs_Products.js
+++ components/Breadcrumbs_Products.js
@@ -3,46 +3,47 @@ import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 
 const Breadcrumbs_Products = ({ businessName }) => {
-  const router = useRouter(); 
-  const [business, setBusiness] = useState('');
+    const router = useRouter();
+    const [business, setBusiness] = useState('');
 
-  useEffect(() => {
-    if (businessName) {
-      setBusiness(businessName);
-    }
-  }, [businessName]);
+    useEffect(() => {
+        if (businessName) {
+            setBusiness(businessName);
+            console.log('Business Name in Breadcrumbs:', businessName); // Log the business name
+        }
+    }, [businessName]);
 
-  return (
-    <nav className="breadcrumb">
-      <ul className="flex space-x-2">
-        <li>
-          <Link href="/">
-            <span className="text-blue-500">Home</span>
-          </Link>
-        </li>
-        <li>
-          <span className="mx-2">/</span>
-          <Link href="/businesses">
-            <span className="text-blue-500">Businesses</span>
-          </Link>
-        </li>
-        {business && (
-          <>
-            <li>
-              <span className="mx-2">/</span>
-              <Link href={`/business/${router.query.id}`}>
-                <span className="text-blue-500">{business}</span>
-              </Link>
-            </li>
-          </>
-        )}
-        <li>
-          <span className="mx-2">/</span>
-          <span>Products</span>
-        </li>
-      </ul>
-    </nav>
-  );
+    return (
+        <nav className="breadcrumb">
+            <ul className="flex space-x-2">
+                <li>
+                    <Link href="/">
+                        <span className="text-blue-500">Home</span>
+                    </Link>
+                </li>
+                <li>
+                    <span className="mx-2">/</span>
+                    <Link href="/businesses">
+                        <span className="text-blue-500">Businesses</span>
+                    </Link>
+                </li>
+                {business && (
+                    <>
+                        <li>
+                            <span className="mx-2">/</span>
+                            <Link href={`/business/${router.query.id}`}>
+                                <span className="text-blue-500">{business}</span>
+                            </Link>
+                        </li>
+                    </>
+                )}
+                <li>
+                    <span className="mx-2">/</span>
+                    <span>Products</span>
+                </li>
+            </ul>
+        </nav>
+    );
 };
 
 export default Breadcrumbs_Products;
